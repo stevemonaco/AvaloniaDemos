6be7008c8bb00f9379c05ac4a61d218e7e3edd6d
Steve Monaco <1782158+stevemonaco@users.noreply.github.com>
2025-01-08 19:56:20 -0500

    Add transparent brush demo

---
+++ TransparentBrushTransitions/MainWindow.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace TransparentBrushTransitions;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TransparentBrushTransitions/TransparentBrushTransition.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia.Animation;
+using Avalonia.Media;
+using Avalonia.Media.Immutable;
+using System;
+
+namespace TransparentBrushTransitions;
+
+/// <summary>
+/// Improves brush color transitions to/from Avalonia's Transparent color (ie. Transparent white, #00FFFFFF)
+/// Only works for ISolidColorBrush
+/// </summary>
+public class TransparentBrushTransition : InterpolatingTransitionBase<IBrush?>
+{
+    protected override IBrush? Interpolate(double progress, IBrush? from, IBrush? to)
+    {
+        if (from is null || to is null)
+            return to ?? from;
+
+        if (from is not ISolidColorBrush fromBrush || to is not ISolidColorBrush toBrush)
+            throw new ArgumentException($"{nameof(TransparentBrushTransition)} bad input arguments");
+
+        if (toBrush.Color == Colors.Transparent) // Interpolate only alpha, use from color channels
+        {
+            var aProgress = (byte)LerpHelpers.Lerp(fromBrush.Color.A, toBrush.Color.A, progress);
+            var a = Math.Clamp(aProgress, (byte)0, (byte)255);
+
+            return new ImmutableSolidColorBrush(Color.FromArgb(a, fromBrush.Color.R, fromBrush.Color.G, fromBrush.Color.B));
+        }
+        else if (fromBrush.Color == Colors.Transparent) // Interpolate only alpha, use to color channels
+        {
+            var aProgress = (byte)LerpHelpers.Lerp(fromBrush.Color.A, toBrush.Color.A, progress);
+            var a = Math.Clamp(aProgress, (byte)0, (byte)255);
+
+            return new ImmutableSolidColorBrush(Color.FromArgb(a, toBrush.Color.R, toBrush.Color.G, toBrush.Color.B));
+        }
+        else // interpolate all channels with same RGB strategy as BrushTransition
+        {
+            return new ImmutableSolidColorBrush(LerpHelpers.LerpColor(fromBrush.Color, toBrush.Color, progress));
+        }
+    }
+}
